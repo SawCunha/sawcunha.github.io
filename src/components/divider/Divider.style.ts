3b8e964fe6415c4b6a84dbdb8a2cc25ae9adb04b
Samuel Cunha <samukasaww@live.com>
2026-05-28 18:30:19 -0300

    fix: Ajustando lint

--- src/components/divider/Divider.style.ts
+++ src/components/divider/Divider.style.ts
@@ -8,7 +8,10 @@ interface Props {
 }
 
 const DividerStyle = styled(DividerMUI)<DividerProps & Props>`
-  background: ${({ $current }) => ($current === true ? 'linear-gradient(180deg, #00f5a0 0%, #00d9f5 100%)' : 'rgba(128, 219, 217, 0.3)')};
+  background: ${({ $current }) =>
+    $current === true
+      ? 'linear-gradient(180deg, #00f5a0 0%, #00d9f5 100%)'
+      : 'rgba(128, 219, 217, 0.3)'};
   ${({ $devicetype }) => ($devicetype === DeviceType.MOBILE ? 'margin-left: 0' : '')};
   ${({ $marginTop }) => ($marginTop === undefined ? '' : `margin-top: ${$marginTop}px`)};
 `;
